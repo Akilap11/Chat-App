6b56708a7dfa9aede7970737f80a1295c3b36918
Akila Tharinda <watlankathilaka@students.nsbm.ac.lk>
2025-02-26 18:13:22 +0530

    chat engine - shutted down

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from "react-chat-engine";
+import './App.css';
+
+const App  = () => {
+    return (
+        <ChatEngine
+        height="100vh"
+        projectID="CHATAPP"
+        userName=""
+        userSecret=""
+        />
+    );
+}
+
+export default App;
